d6d13c6f7be4fb469bad79bffdb3bdb5e402adae
Hamza Rashed <hamzarashed503@gmail.com>
2020-08-04 04:28:22 -0700

    first

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+let port = 3000
+
+app.use(express.json({limit:'50mb'}));
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('./files/'));
+app.get("/" , (req , res)=>{
+    res.sendFile(__dirname + '/files/index.html')
+})
+
+
+app.listen(port , ()=>{
+    console.log('server is running')
+})
